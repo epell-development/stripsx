97424b8a236ddf3da766d4228d7b3515e7f9715a
SnivyCode <71209068+SnivyCode@users.noreply.github.com>
2021-10-03 18:04:08 +0200

    Website

---
+++ script.js
@@ -0,0 +1,9 @@
+function validate() {
+    var input = document.getElementById("input").value;
+    var password = "malpensa24"
+    
+    if(input==password) {
+        window.open("pages/strips.html", "_self")
+        
+    } else alert("Password not correct.")
+}
